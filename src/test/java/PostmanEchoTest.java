e84d44c3364cbe53e9fa8d7740fa25b7fddd0dcc
Denis Dmitriev <denitos97@gmail.com>
2021-03-29 16:08:26 +0300

    Created test

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void testPostmanEcho() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
