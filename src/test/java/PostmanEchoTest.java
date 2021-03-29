1107899e43bec5d0b78772574f31d3d11ceacd10
Denis Dmitriev <denitos97@gmail.com>
2021-03-29 21:06:12 +0300

    Fixing test

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("date", equalTo("some data"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
